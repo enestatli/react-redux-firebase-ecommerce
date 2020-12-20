c1def9b02ba8c3a91da2d60e8d8087442759b220
Hakan Han <eenestatli@gmail.com>
2020-12-20 17:50:45 +0300

    improved product card

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -2,20 +2,48 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { fetchProductStart } from '../../redux/Products/actions';
+
+import Button from './../../components/Forms/Button';
 import './styles.scss';
 
 const ProductCard = ({}) => {
   const dispatch = useDispatch();
-  const { productName } = useSelector((state) => state.productsData.product);
+  const { productName, productThumbnail, productPrice } = useSelector(
+    (state) => state.productsData.product
+  );
   const { productID } = useParams();
 
   useEffect(() => {
     dispatch(fetchProductStart(productID));
   }, []);
 
+  const configAddtoCartBtn = {
+    type: 'button',
+  };
+
   return (
     <div className="productCard">
-      <h1>{productName}</h1>
+      <div className="hero">
+        <img src={productThumbnail} alt={productName} />
+      </div>
+
+      <div className="productDetails">
+        <ul>
+          <li>
+            <h1>{productName}</h1>
+          </li>
+
+          <li>
+            <h1>${productPrice}</h1>
+          </li>
+
+          <li>
+            <div className="addToCart">
+              <Button {...configAddtoCartBtn}>Add to cart</Button>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
