d68084672755bd106f8528d10b9958c8e9c3d38b
Hakan Han <eenestatli@gmail.com>
2020-12-20 16:39:57 +0300

    rendering product detail

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,7 +1,23 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { fetchProductStart } from '../../redux/Products/actions';
 import './styles.scss';
 
 const ProductCard = ({}) => {
-  return <div className="productCard">TESTTTTTTTTTT</div>;
+  const dispatch = useDispatch();
+  const { productName } = useSelector((state) => state.productsData.product);
+  const { productID } = useParams();
+
+  useEffect(() => {
+    dispatch(fetchProductStart(productID));
+  }, []);
+
+  return (
+    <div className="productCard">
+      <h1>{productName}</h1>
+    </div>
+  );
 };
 
 export default ProductCard;
