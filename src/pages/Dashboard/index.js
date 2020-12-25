8d8bd9d985073ee1d70171be3dcd927dda4325c7
Hakan Han <eenestatli@gmail.com>
2020-12-25 17:09:49 +0300

    fetching user order history

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,20 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserOrderHistory } from '../../redux/Orders/actions';
 import './styles.scss';
 
-const Dashboard = (props) => {
+const mapState = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
+const Dashboard = () => {
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector(mapState);
+
+  useEffect(() => {
+    dispatch(getUserOrderHistory(currentUser.id));
+  }, []);
+
   return (
     <div>
       <h1>Dashboard</h1>
